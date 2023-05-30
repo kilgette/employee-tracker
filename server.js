70576bc2d44bb86ac8939feabf2a7f817c5eb02d
kilgette <kilgette@gmail.com>
2023-05-29 21:54:04 -0500

    comment out console.table - it worksgit add -A

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
-const cTable = require('console.table');
+// const cTable = require('console.table');
 
 const selectDepartmentSQL = "SELECT * FROM DEPARTMENT;";
 const selectRoleSQL = "SELECT * FROM ROLE;";
